44da6be2821b788e411098bc64035907a608d43e
Hudson Poissant <hudson.poissant@brooksrunning.com>
2021-04-27 14:54:49 -0700

    Add ratings component and style

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = (props) => {
   return (
@@ -15,9 +16,10 @@ const Product = (props) => {
           </Card.Title>
         </a>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {props.product.rating} from {props.product.numReviews} reviews
-          </div>
+          <Rating
+            value={props.product.rating}
+            text={`${props.product.numReviews} reviews`}
+          />
         </Card.Text>
 
         <Card.Text as='h3'>${props.product.price}</Card.Text>
